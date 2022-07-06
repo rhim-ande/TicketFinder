d77d1758fa57be0422aa8be3339f8c935843d1d6
Rhim Andemichael <rhima@princeton.edu>
2022-07-06 21:24:41 +0000

    ticketmaster working

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # TicketFinder
 
-**Install the following libraries**
+**Install the following libraries:**
 1. requests
 2. pandas as pd 
 3. sqalchemy as db
 4. from sqlalchemy import create_engine 
-5. import googleapiclient.discovery        
-6. from googleapiclient.discovery import build
-7. json
+5. json
+6. import os
 
-**Directions**
+**Directions:**
 1. Enter one or more of the following criteria in the terminal: 
     A. Artist Name 
     B. City 
--- TicketFinder.py
+++ TicketFinder.py
@@ -1,9 +1,56 @@
-GET /discovery/v2/events.json?apikey={HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g}&size=1 HTTP/1.1
-Host: app.ticketmaster.com
-X-Target-URI: https://app.ticketmaster.com
-Connection: Keep-Alive
+import requests
+import pprint
+import json
+import os
+#import pandas as pd
+#import sqlalchemy as db
+#from sqlalchemy import create_engine 
+import ticketpy
 
 
-url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey={HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g}"
 
-response = requests.get(url)
+
+#message: enter information at the prompt. All prompts must be answered. 
+
+keyword_preference = input("Enter keyword:")
+state_preference = input("Enter US state code:")
+date_preference = input("Enter start date in the format 'yyyy-mm-dd' ")
+
+
+
+#apikey = "HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g"
+tm_client = ticketpy.ApiClient("HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g") 
+
+pages = tm_client.events.find(
+    classification_name='Hip-Hop',
+    state_code='GA',
+    start_date_time='2022-07-19T20:00:00Z',
+    end_date_time='2022-07-21T20:00:00Z'
+)
+
+for page in pages:
+    print(page)
+    print(pages)
+    for event in page:
+        print(event)
+
+
+
+#url = "https://app.ticketmaster.com/discovery/v2/events.json?{apikey}"
+#url = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey={HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g}"
+#url= 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey={apikey}'
+
+#response = requests.get(url)
+
+#print(response)
+
+
+
+#if keyword_preference == keyword:
+#   print(event)
+
+#if city_preference == city:
+#   print(event)
+
+
+
