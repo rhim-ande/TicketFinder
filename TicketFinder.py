2e9161041279fdda6d1e165d753f1fe378d98929
Rhim Andemichael <rhima@princeton.edu>
2022-07-06 23:13:51 +0000

    data printing

--- TicketFinder.py
+++ TicketFinder.py
@@ -2,37 +2,45 @@ import requests
 import pprint
 import json
 import os
-#import pandas as pd
-#import sqlalchemy as db
-#from sqlalchemy import create_engine 
 import ticketpy
 
+
+apikey = "HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g"
+#url = 'https://app.ticketmaster.com/discovery/v2/events'
+#response = requests.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=4dsfsf94tyghf85jdhshwge334')
+#print(response)
+
+response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g")
+print(response.json())
 
 
 
 #message: enter information at the prompt. All prompts must be answered. 
 
-keyword_preference = input("Enter keyword:")
-state_preference = input("Enter US state code:")
-date_preference = input("Enter start date in the format 'yyyy-mm-dd' ")
+#keyword_preference = input("Enter keyword:")
+#state_preference = input("Enter US state code:")
+#date_preference = input("Enter start date in the format 'yyyy-mm-dd' ")
 
 
 
-#apikey = "HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g"
-tm_client = ticketpy.ApiClient("HYTeCD8nCfFuOtQog8WunqWnAgA5eS1g") 
+tm_client = ticketpy.ApiClient(apikey) 
 
 pages = tm_client.events.find(
     classification_name='Hip-Hop',
-    state_code='GA',
+    state_code='NY',
     start_date_time='2022-07-19T20:00:00Z',
     end_date_time='2022-07-21T20:00:00Z'
 )
 
-for page in pages:
-    print(page)
-    print(pages)
-    for event in page:
-        print(event)
+
+#for page in pages:
+    #for event in page:
+       # print(event)
+
+
+
+        
+
 
 
 
